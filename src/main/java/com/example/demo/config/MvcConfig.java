96a18785bb75b2a080c4c9e14a1ce3a860b67617
Kevin Sheppard <ksheppard@mpu.gov.tt>
2018-05-23 23:14:38 -0400

    Got the app to look in multiple locations for resources

--- src/main/java/com/example/demo/config/MvcConfig.java
+++ src/main/java/com/example/demo/config/MvcConfig.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
   @Value("${spring.resources.static-locations}")
-  String resourceLocations;
+  String[] resourceLocations;
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
